3374113bd39740b0d1311bb2b7a0ad4637e22ed9
kumaradji <kumaradji@gmail.com>
2023-01-18 20:33:36 +0300

    несколько тестов 18 января

--- codewars_tasks_01/test_codewars 02.py
+++ codewars_tasks_01/test_codewars 02.py
@@ -19,28 +19,29 @@
 #     else:
 #         print('НЕТ')
 #
-s = 'ZpglnRxqenU'
-
-
-def accum(s):
-    str = [char for char in s]
-    res = []
-    a = 0
-    for x in str:
-        a += 1
-        b = x * a
-        b = b.title()
-        res.append(b)
-    res = '-'.join(res)
-
-    return res
-
-
-print(accum(s))
+# s = 'ZpglnRxqenU'
+#
+#
+# def accum(s):
+#     str = [char for char in s]
+#     res = []
+#     a = 0
+#     for x in str:
+#         a += 1
+#         b = x * a
+#         b = b.title()
+#         res.append(b)
+#     res = '-'.join(res)
+#
+#     return res
+#
+#
+# print(accum(s))
 
 # def accum(s):
 #     return '-'.join((a * i).title() for i, a in enumerate(s, 1))
 
 # def accum(s):
 #     return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
+
 
