8237f5e10399655e6fc6a8b97b5eb7515d9b2203
kumaradji <kumaradji@me.com>
2023-01-20 22:50:19 +0300

    codewars tasks from 6kyu

--- codewars_tasks_01/test_codewars 02.py
+++ codewars_tasks_01/test_codewars 02.py
@@ -71,16 +71,103 @@
 # def fake_bin(x):
 #     return ''.join('0' if c < '5' else '1' for c in x)
 
-def sum_mix(arr):
-    for x in arr:
-        res = []
-        i = 0
-        x = int(x)
-        y = x[i] + x[i + 1]
-        i += 1
-    return res.append(y)
+# '''
+# Реализуйте функцию unique_in_order, которая принимает в качестве аргумента
+# последовательность и возвращает список элементов без каких-либо элементов
+# с одинаковым значением рядом друг с другом и сохраняет исходный порядок элементов.
+#
+# '''
+#
+# sequence = "AAAABBBCCDAABBB"
+#
+#
+# def unique_in_order(sequence):
+#     chars = []
+#     for i in range(len(sequence)):
+#         if i == 0 or sequence[i] != sequence[i - 1]:
+#             chars.append(sequence[i])
+#     return chars
+#
+#
+# def unique_in_order(iterable):
+#     res = []
+#     for item in iterable:
+#         if len(res) == 0 or item != res[-1]:
+#             res.append(item)
+#     return res
+#
+#
+# print(unique_in_order(sequence))
 
 
-print(sum_mix(arr))
+# '''Завершите функцию так, чтобы она нашла среднее значение
+# из трех переданных ей баллов и вернула
+# буквенное значение, связанное с этой оценкой.'''
+# def get_grade(s1, s2, s3):
+#     score = (s1 + s2 + s3) / 3
+#     if 90 <= score <= 100:
+#         return "A"
+#     elif 80 <= score < 90:
+#         return "B"
+#     elif 70 <= score < 80:
+#         return "C"
+#     elif 60 <= score < 70:
+#         return "D"
+#     elif 0 <= score < 60:
+#         return "F"
+#
+#
+# print(get_grade(100, 85, 96))
+#
+# '''Вы, вероятно, знаете систему "лайков" на Facebook и
+# других страницах. Люди могут "лайкать" записи в блоге,
+# фотографии или другие элементы. Мы хотим создать текст,
+# который должен отображаться рядом с таким элементом.
+#
+# Реализуйте функцию, которая принимает массив, содержащий
+# имена людей, которым нравится элемент. Он должен
+# возвращать отображаемый текст, как показано в примерах:
+#
+# [] --> "это никому не нравится"
+# ["Питер"]                         --> "Питеру это нравится"
+# ["Джейкоб", "Алекс"] --> "Джейкоб и Алекс вот так"
+# ["Макс", "Джон", "Марк"] --> "Макс, Джон и Марк вот так"
+# ["Алекс", "Джейкоб", "Марк", "Макс"] --> "Алекс, Джейкоб и еще 2 таких же"
+# Примечание: Для 4 или более имен число в "и 2 других" просто увеличивается.
+# '''
+#
+# names = ['Alex', 'Jacob', 'Mark', 'Max', 'Alex', 'Jacob', 'Mark', 'Max']
 
 
+# def likes(names):
+#     n = len(names)
+#
+#     if names == []:
+#         return 'no one likes this'
+#     elif n == 1:
+#         return f'{names[0]} likes this'
+#     elif n == 2:
+#         return f'{names[0]} and {names[1]} like this'
+#     elif n == 3:
+#         return f'{names[0]}, {names[1]} and {names[2]} like this'
+#     elif n == 4:
+#         return f'{names[0]}, {names[1]} and 2 others like this'
+#     elif n > 4:
+#         return f'{names[0]}, {names[1]} and {n - 2} others like this'
+#
+#
+# print(likes(names))
+
+
+# def likes(names):
+#     n = len(names)
+#     return {
+#         0: 'no one likes this',
+#         1: '{} likes this',
+#         2: '{} and {} like this',
+#         3: '{}, {} and {} like this',
+#         4: '{}, {} and {others} others like this'
+#     }[min(4, n)].format(*names[:3], others=n - 2)
+#
+#
+# print(likes(names))
