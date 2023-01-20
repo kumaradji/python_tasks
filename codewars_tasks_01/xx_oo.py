1677e3dd0ef3c97151b3e069929e3bfc811b76a5
kumaradji <kumaradji@gmail.com>
2023-01-20 21:01:26 +0300

    init

--- codewars_tasks_01/xx_oo.py
+++ codewars_tasks_01/xx_oo.py
@@ -7,7 +7,6 @@ def draw_board(board):
         print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
         print("-" * 13)
 
-
 def take_input(player_token):
     valid = False
     while not valid:
