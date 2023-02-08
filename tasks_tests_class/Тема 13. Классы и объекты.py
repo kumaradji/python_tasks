103ab14af6cab1742dcb87950d8bba24f0bceb0b
kumaradji <121006890+kumaradji@users.noreply.github.com>
2023-02-08 17:56:53 +0300

    Update Тема 13. Классы и объекты.py
    убран 2.5.8

--- tasks_tests_class/Тема 13. Классы и объекты.py
+++ tasks_tests_class/Тема 13. Классы и объекты.py
@@ -46,39 +46,3 @@
 # triangle4 = TriangleChecker([77, -3, 4])
 # print(triangle4.is_triangle())
 #
-'''
-Задание 2.5.8
-Создайте вычисляемое свойство для класса Square.
-
-Сделайте метод по вычислению площади свойством.
-Сделайте сторону квадрата свойством, которое можно
-установить только через сеттер.
-В сеттере добавьте проверку условия, что сторона
-должна быть неотрицательной.
-'''
-
-
-class Square:
-    _side = None
-
-    def __init__(self, side):
-        if side > 0:
-            self._side = side
-
-    @property  # создаем свойство side при помощи декоратора
-    def side(self):
-        return self._side
-
-    @side.setter
-    def side(self, side):
-        if side >= 0:
-            self._side = side
-
-    @property
-    def square(self):
-        return self._side ** 2
-
-
-if __name__ == "__main__":
-    q = Square(22)
-print(q.square)
